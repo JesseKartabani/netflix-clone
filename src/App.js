72be3c3cfe70f2f5452f928d7ced499fa2aa5607
Jesse1224 <jessekartabani@gmail.com>
2022-12-07 09:51:45 +1100

    Move components into own folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Row from "./Row";
+import Row from "./Components/Row";
 import requests from "./requests";
-import Banner from "./Banner";
-import Nav from "./Nav";
+import Banner from "./Components/Banner";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
