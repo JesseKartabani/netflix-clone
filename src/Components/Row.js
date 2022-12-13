7a802e7a6d034ce581b06cfdda8c15d27a2bc947
Jesse1224 <jessekartabani@gmail.com>
2022-12-13 16:09:13 +1100

    Blur movie images until they are loaded

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import axios from "../axios";
 import "./Row.css";
 import Youtube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import CircularProgress from "@mui/material/CircularProgress";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -41,23 +44,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
   };
 
   return (
-    <div className="row">
-      <h2>{title}</h2>
-      <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+    <Suspense fallback={<CircularProgress />}>
+      <div className="row">
+        <h2>{title}</h2>
+        <div className="row__posters">
+          {movies.map((movie) => (
+            <LazyLoadImage
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "row__posterLarge"} ${
+                !imageLoaded && "blurred__image"
+              }`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              onLoad={() => setImageLoaded(true)}
+            />
+          ))}
+        </div>
+        {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
       </div>
-      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
-    </div>
+    </Suspense>
   );
 }
 
